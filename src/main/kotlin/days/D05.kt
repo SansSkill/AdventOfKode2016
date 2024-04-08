9ab6ef6ddea5637768decdcc4ea249d890c2b730
Jasper den Brok <jasper.den.brok@gmail.com>
2024-04-09 01:24:04 +0200

    Day 5

--- src/main/kotlin/days/D05.kt
+++ src/main/kotlin/days/D05.kt
@@ -1,8 +1,34 @@
 package days
 
+import java.math.BigInteger
+import java.security.MessageDigest
+
 @Suppress("unused")
 object D05 : Day {
-    override suspend fun part1(): String = "TODO"
+    private val md5 = MessageDigest.getInstance("MD5")
+    private val prefix = readFile()[0]
 
-    override suspend fun part2(): String = "TODO"
+    override suspend fun part1(): String =
+        buildString {
+            var n = 0
+            while (length < 8) {
+                val hash = md5("$prefix${n++}")
+                if (hash.startsWith("00000")) append(hash[5])
+            }
+        }
+
+    override suspend fun part2(): String {
+        val charArray = CharArray(8).apply { fill('.') }
+        var n = 0
+        while (charArray.any { c -> c == '.' }) {
+            val hash = md5("$prefix${n++}")
+            if (hash.startsWith("00000") && hash[5] in '0'..'7') {
+                val i = hash[5] - '0'
+                if (charArray[i] == '.') charArray[i] = hash[6]
+            }
+        }
+        return charArray.joinToString("")
+    }
+
+    private fun md5(input:String): String = BigInteger(1, md5.digest(input.toByteArray())).toString(16).padStart(32, '0')
 }
