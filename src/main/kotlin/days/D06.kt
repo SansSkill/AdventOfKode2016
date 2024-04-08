15ce841f5d65c26ecc504af6abbf87df219b108d
Jasper den Brok <jasper.den.brok@gmail.com>
2024-04-09 01:28:24 +0200

    Day 6

--- src/main/kotlin/days/D06.kt
+++ src/main/kotlin/days/D06.kt
@@ -2,7 +2,17 @@ package days
 
 @Suppress("unused")
 object D06 : Day {
-    override suspend fun part1(): String = "TODO"
+    private val input = readFile()
 
-    override suspend fun part2(): String = "TODO"
+    override suspend fun part1(): String {
+        val arrs = Array(input[0].length) { IntArray(26) }
+        for (s in input) for (i in s.indices) arrs[i][s[i] - 'a']++
+        return arrs.map { arr -> arr.indices.maxBy(arr::get).plus('a'.code).toChar() }.joinToString("")
+    }
+
+    override suspend fun part2(): String {
+        val arrs = Array(input[0].length) { IntArray(26) }
+        for (s in input) for (i in s.indices) arrs[i][s[i] - 'a']++
+        return arrs.map { arr -> arr.indices.minBy(arr::get).plus('a'.code).toChar() }.joinToString("")
+    }
 }
