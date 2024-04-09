c8d9210b72b47681101eca5cacabed968c47ca62
Jasper den Brok <jasper.den.brok@gmail.com>
2024-04-09 18:00:54 +0200

    Day 9

--- src/main/kotlin/days/D09.kt
+++ src/main/kotlin/days/D09.kt
@@ -2,7 +2,25 @@ package days
 
 @Suppress("unused")
 object D09 : Day {
-    override suspend fun part1(): String = "TODO"
+    private val input = readFile()[0]
 
-    override suspend fun part2(): String = "TODO"
+    private fun getDecompressedLength(s: String, recursive: Boolean): Long {
+        var count = 0L
+        var i = 0
+        while (i < s.length) {
+            if (s[i] != '(') { count++; i++ }
+            else {
+                val idx = s.indexOf(')', startIndex = i)
+                val (compressedLength, times) = s.substring(i + 1, idx).split("x").map(String::toInt)
+                val decompressedLength = if (recursive) getDecompressedLength(s.substring(idx + 1, idx + 1 + compressedLength), true) else compressedLength.toLong()
+                i = idx + compressedLength + 1
+                count += decompressedLength * times
+            }
+        }
+        return count
+    }
+
+    override suspend fun part1(): String = getDecompressedLength(input, false).toString()
+
+    override suspend fun part2(): String = getDecompressedLength(input, true).toString()
 }
