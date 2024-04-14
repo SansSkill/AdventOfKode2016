efe9d4e594f159d16bbad827ed6cfd23e6c33a0a
Jasper den Brok <jasper.den.brok@gmail.com>
2024-04-15 01:41:48 +0200

    Day 16

--- src/main/kotlin/days/D16.kt
+++ src/main/kotlin/days/D16.kt
@@ -2,7 +2,27 @@ package days
 
 @Suppress("unused")
 object D16 : Day {
-    override fun part1(): String = "TODO"
+    private val initialState = readFile()[0]
 
-    override fun part2(): String = "TODO"
+    override fun part1(): String = getCheckSum(dragonCurve(272))
+
+    override fun part2(): String = getCheckSum(dragonCurve(35651584))
+
+    private fun getCheckSum(s: String): String {
+        val checksum = buildString {
+            for (i in s.indices step 2) if (s[i] == s[i + 1]) append('1') else append('0')
+        }
+        return if (checksum.length and 1 == 0) getCheckSum(checksum) else checksum
+    }
+
+    private fun dragonCurve(minimumLength: Int): String =
+        buildString {
+            append(initialState)
+            while (length < minimumLength) {
+                val currentLength = length
+                append('0')
+                for (i in currentLength - 1 downTo 0) if (this[i] == '0') append('1') else append('0')
+            }
+            delete(minimumLength, length)
+        }
 }
