5d0ce6be51d6e6e5042f4122f86e0e6042b25a47
Jasper den Brok <jasper.den.brok@gmail.com>
2024-04-15 18:18:29 +0200

    Day 18

--- src/main/kotlin/days/D18.kt
+++ src/main/kotlin/days/D18.kt
@@ -2,7 +2,28 @@ package days
 
 @Suppress("unused")
 object D18 : Day {
-    override fun part1(): String = "TODO"
+    private val initialState: BooleanArray = readFile()[0].map { c -> c == '^' }.toBooleanArray()
 
-    override fun part2(): String = "TODO"
+    override fun part1(): String = getSafeTiles(40).toString()
+
+    override fun part2(): String = getSafeTiles(400000 ).toString()
+
+    private fun getSafeTiles(rows: Int): Int {
+        var row = initialState
+        var count = row.count { b -> !b }
+        repeat(rows - 1) {
+            row = generateNextRow(row)
+            count += row.count { b -> !b }
+        }
+        return count
+    }
+
+    private fun generateNextRow(row: BooleanArray): BooleanArray {
+        val arr = BooleanArray(row.size)
+        arr[0] = row[1]
+        arr[arr.lastIndex] = row[row.lastIndex - 1]
+        for (i in 1..< row.lastIndex)
+            arr[i] = row[i - 1] xor row[i + 1]
+        return arr
+    }
 }
