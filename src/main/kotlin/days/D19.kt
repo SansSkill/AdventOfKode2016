83dea5843cc5a42966fa99769cebf3dbca29ea92
Jasper den Brok <jasper.den.brok@gmail.com>
2024-04-15 18:18:35 +0200

    Day 19

--- src/main/kotlin/days/D19.kt
+++ src/main/kotlin/days/D19.kt
@@ -1,8 +1,31 @@
 package days
 
+import java.util.LinkedList
+
 @Suppress("unused")
 object D19 : Day {
-    override fun part1(): String = "TODO"
+    private val numberOfElves: Int = readFile()[0].toInt()
 
-    override fun part2(): String = "TODO"
+    override fun part1(): String {
+        val queue = LinkedList<Int>()
+        queue.addAll(1..numberOfElves)
+        while (queue.size > 1) {
+            queue.add(queue.poll())
+            queue.poll()
+        }
+        return queue.poll().toString()
+    }
+
+    override fun part2(): String {
+        val list = mutableListOf<Int>()
+        list.addAll(1..numberOfElves)
+        var p = 0
+        while (list.size > 1) {
+            val indexToRemove = (p + list.size / 2) % list.size
+            list.removeAt(indexToRemove)
+            if (indexToRemove > p) p++
+            if (p == list.size) p = 0
+        }
+        return list[0].toString()
+    }
 }
