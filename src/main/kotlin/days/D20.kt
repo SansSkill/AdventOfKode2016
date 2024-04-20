9726976abb21d4299b0788fc24f321beac33ff26
Jasper den Brok <jasper.den.brok@gmail.com>
2024-04-20 13:12:22 +0200

    Day 20

--- src/main/kotlin/days/D20.kt
+++ src/main/kotlin/days/D20.kt
@@ -2,7 +2,25 @@ package days
 
 @Suppress("unused")
 object D20 : Day {
-    override fun part1(): String = "TODO"
+    private val ranges: List<LongRange>
 
-    override fun part2(): String = "TODO"
+    init {
+        val unmergedRanges = readFile().map { s -> s.split("-").let { (ls, rs) -> ls.toLong()..rs.toLong() } }.sortedWith(compareBy(LongRange::first).thenBy(LongRange::last))
+        ranges = buildList {
+            add(unmergedRanges[0])
+            for (i in 1 ..< unmergedRanges.size) {
+                val nextRange = unmergedRanges[i]
+                val prevRange = get(lastIndex)
+                if (nextRange.first > prevRange.last + 1) add(nextRange)
+                else {
+                    removeLast()
+                    add(prevRange.first..prevRange.last.coerceAtLeast(nextRange.last))
+                }
+            }
+        }
+    }
+
+    override fun part1(): String = ranges[0].last.inc().toString()
+
+    override fun part2(): String = 4294967295L.inc().minus(ranges.sumOf { range -> range.last - range.first + 1 }).toString()
 }
